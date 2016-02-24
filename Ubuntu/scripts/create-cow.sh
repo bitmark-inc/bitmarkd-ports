7d7b1255ebdc5be6ae133d87414ccffc935bf47a
Christopher Hall <hsw@bitmark.com>
2016-02-24 14:41:13 +0800

    some files for Ubuntu builds
    Signed-off-by: Christopher Hall <hsw@bitmark.com>

---
+++ Ubuntu/scripts/create-cow.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+# create or update the cowuider base for a specific dist and arch
+
+# settings
+dist=wily
+arch=amd64
+
+# errors
+ERROR()
+{
+  echo error: $*
+  exit 1
+}
+
+# base directory
+base="/var/cache/pbuilder/${dist}-${arch}"
+[ -d "${base}" ] || sudo mkdir -p "/var/cache/pbuilder/${dist}-${arch}"
+
+# base image file
+cow="${base}/base.cow"
+
+# create or update the base image
+if [ ! -f "${cow}" ]
+then
+  # create new chroot base image
+  sudo cowbuilder --create --basepath "/var/cache/pbuilder/${dist}-${arch}/base.cow" --distribution squeeze --debootstrapopts --arch --debootstrapopts "${arch}"
+  [ $? -ne 0 ] && ERROR "cowbuilder --create failed"
+
+else
+
+  # update the chroot
+  #   setting HOME is necessary as sudo strips environment variables for security reasons
+  #   otherwise cowbuilder will not find the ~/.pbuilderrc
+  [ X"${created}" = X"no" ] && sudo HOME="${HOME}" DIST="${dist}" ARCH="${arch}" cowbuilder --update
+
+fi
+
+# Eample of builing a specific package
+#
+#   # build a package
+#   dget -x http://ftp.de.debian.org/debian/pool/main/n/nano/nano_2.2.6-1.dsc
+#
+#   # build a package
+#   sudo DIST="${dist}" ARCH="${arch}" cowbuilder --build nano_2.2.6-1.dsc
+#
+#   # result will be available in: /var/cache/pbuilder/${dist}-${arch}/result
+#   ls -al "/var/cache/pbuilder/${dist}-${arch}/result"
---
+++ Ubuntu/scripts/create-src-archive.sh
@@ -0,0 +1,143 @@
+#!/bin/sh
+# scan all dependencies of the current git repo and produce a set of archives
+# then tar into a master archive
+# this script must be in the root directory of this repository to work
+
+this_dir=$(dirname "$0")
+
+ERROR()
+{
+  echo error: $*
+  exit 1
+}
+
+# set initial directory
+cd "${this_dir}" || ERROR "cannot change to directory: ${this_dir}"
+this_dir="${PWD}"
+
+# need this to work
+libucl=vstakhov-libucl-0.7.3_GH0.tar.gz
+libucl_url=https://codeload.github.com/vstakhov/libucl/tar.gz/0.7.3
+libucl_dir="${this_dir}/cache"
+
+# fetch and cache libucl source
+mkdir -p "${libucl_dir}"
+[ -f "${libucl_dir}/${libucl}" ] || curl -o  "${libucl_dir}/${libucl}" "${libucl_url}"
+
+# define supported projects
+project_list='bitmarkd miniature-spoon'
+account_path=github.com/bitmark-inc
+go_src_dir="${GOPATH}/src"
+
+[ -d "${go_src_dir}" ] || ERROR "missing GO source dir: ${go_src_dir}"
+
+cd_or_error()
+{
+  [ -d "$1" ] || ERROR "not a directory: $1"
+  cd "$1" || ERROR "cannot change directory to:: $1"
+}
+
+make_mega_package()
+{
+  local account_path project
+  account_path="$1"; shift
+  project="$1"; shift
+
+  local version debian version
+  version='0.0.0'
+  debian_version=1
+
+  echo "processing project: ${project}"
+
+  # find project root
+  local project_dir
+  project_dir="${go_src_dir}/${account_path}/${project}"
+  cd_or_error "${project_dir}"
+
+  # check that there is a debian directory
+  local debian_dir
+  debian_dir="${project_dir}/debian"
+  if [ ! -d "${debian_dir}" ]
+  then
+    echo "skipping: ${project}"
+    echo "not a debian directory: ${debian_dir}"
+    return 1
+  fi
+
+  # compute all dependencies and extract unique items
+  local depends
+  depends=$(go list -f '{{join .Deps "\n"}}' . | (
+             while read dep
+             do
+               dir="${GOPATH}/src/${dep}"
+               [ -d "${dir}" ] && (cd "${dir}" && git rev-parse --show-toplevel)
+             done
+           ) | sort -u)
+
+  # to cache the individual archives
+  local archive_dir
+  archive_dir="${this_dir}/archives"
+  rm -rf "${archive_dir}"
+  mkdir -p "${archive_dir}"
+
+  # install the libucl sources
+  [ -f "${archive_dir}/${libucl}" ] || cp -p "${libucl_dir}/${libucl}" "${archive_dir}"
+
+  # build individual packages
+  local packages fullpath repo account hash file
+  packages=''
+  for fullpath in ${depends}
+  do
+    echo processing: ${fullpath}
+    repo=$(basename "${fullpath}")
+    account=$(basename $(dirname "${fullpath}"))
+
+    cd "${fullpath}" || ERROR "cannot change directory to: ${fullpath}"
+
+    hash=$(git log -n 1 --pretty='%h')
+
+    echo detected: ${account}/${repo} hash: ${hash}
+    packages="${packages} github.com/${account}/${repo}:${hash}"
+
+    file="${archive_dir}/${account}-${repo}-${hash}_GH0.tar.gz"
+    if [ -f "${file}" ]
+    then
+      echo "skipping: ${file}"
+    else
+      echo "creating: ${file}"
+      git archive --format=tar.gz --prefix="${repo}-${hash}/" "${hash}" -o "${file}"
+    fi
+
+  done
+
+  # auto detect version? from git tags perhaps
+  # hash=$(git log -n 1 --pretty='%h')
+
+  # make the mega-package
+  local master_file
+  master_file="${this_dir}/${project}_${version}+dfsg${debian_version}.orig.tar.gz"
+  rm -f "${master_file}"
+  echo "creating: ${master_file}"
+  tar czf "${master_file}" -C "${archive_dir}" .
+
+  # setup Debian structure
+  local versions_mk
+  versions_mk="${debian_dir}/versions.mk"
+  rm -f "${versions_mk}"
+  for p in ${packages}
+  do
+    printf 'GO_PACKAGES += %s\n' "${p}" >> "${versions_mk}"
+  done
+
+  printf 'LIBUCL = %s\n' "${libucl}" >> "${versions_mk}"
+
+  printf 'APP_NAME = %s\n' "${project}" >> "${versions_mk}"
+
+  cd_or_error "${this_dir}"
+}
+
+# create all projects
+for project in ${project_list}
+do
+  make_mega_package "${account_path}" "${project}"
+done
