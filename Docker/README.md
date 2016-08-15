ce13839dee711248daac88b7fed9e8f065d41669
Yun-Tai Chang <yun-tai@bitmark.com>
2016-08-15 14:32:41 +0800

    remove old script and update readme file

--- Docker/README.md
+++ Docker/README.md
@@ -10,8 +10,7 @@
 - cpu-miner: (optional) Mine Bitmark blocks
 
 ## Chain
-Bitmark provide three diffrent chains to let the `bitmarkd` join in. They are `local`, `testing`, `bitmark`.
-- `local`: create private bitmark network and bitcoin
+Bitmark provide three diffrent chains to let the `bitmarkd` join in. They are `testing`, `bitmark`.
 - `testing`: link to public test bitmark network, to pay the transactions, please contact us
 - `bitmark`: link to public bitmark network, pay the transactions with real bitcoin
 
@@ -19,7 +18,6 @@ Here is a table to indicate what bitmark chain corresponds to what bitcoin chain
 ```
   bitmark chain |   bitcoin chain
 ================+========================
-  local         |   regtest
   testing       |   regtest (from bitmark)
   bitmark       |   livenet
 ```
@@ -39,17 +37,17 @@ Here is the command looks like, you can just copy-paste this command line, or ch
 
 For Linux user
 ```
-sudo docker run --detach --name bitmarkNode -p 2160:2160 -p 2150:2150 -p 2140:2140 -p 2130:2130 bitmark/bitmark-node bitmark-webgui -c /etc/bitmark-webgui.conf start
+sudo docker run --detach --name bitmarkNode -p 2150:2150 -p 2140:2140 -p 2130:2130 bitmark/bitmark-node bitmark-webgui -c /etc/bitmark-webgui.conf start
 ```
 For Mac user, you need to run this command to start the bitmark-webgui
 ```
-sudo docker run --detach --name bitmarkNode -P -p 2160:2160 -p 2150:2150 -p 2140:2140 -p 2130:2130 bitmark/bitmark-node bitmark-webgui -c /etc/bitmark-webgui.conf start
+sudo docker run --detach --name bitmarkNode -P -p 2150:2150 -p 2140:2140 -p 2130:2130 bitmark/bitmark-node bitmark-webgui -c /etc/bitmark-webgui.conf start
 ```
 
 The options meaning:
 - `--detach`: run the container in background
 - `--name bitmarkNode`: name the container bitmarkNode. You can modify the name
-- `-p 2160:2160 -p 2150:2150`: expose the container port 2150 to host port 2150. This port is for `bitamrk-webgui`. You can change the host port by replacing `2150:2150` to `port-u-want:2150`
+- `-p 2150:2150`: expose the container port 2150 to host port 2150. This port is for `bitamrk-webgui`. You can change the host port by replacing `2150:2150` to `port-u-want:2150`
 - `-p 2140:2140`: expose the container port 2140. This port is for `cpu-miner`
 - `-p 2130:2130`: expose the container port 2130. This port is for `bitmarkd`
 - `bitmark-webgui -c /etc/bitmark-webgui.conf start`: command to start `bitmark-webgui`
@@ -58,93 +56,28 @@ Open the `https://localhost:2150` or `https://host-ip:2150` or `https://dockerVM
 
 Some browsers (such as Chrome) will show you message like "This site can’t provide a secure connection"  because we use self signed certification. Please use FireFox and then click `Advanced` -> `Add Exception...` -> uncheck `Permanently store this exception` -> `Confirm Security Exception`. Then it will show you a login page.
 
-## Setup through web GUI
-Before you start, make a decision of what bitmark chain you would like to use. Here are some requirements for different chain.
-- `Local`: if you want to run your own bitmark system, choose this bitmark chain
-- `Testing`: this chain provides you a playground of bitmark, like bitcoin testnet
-- `Bitmark`: connect to real bitmark network, issue and transfer your digital property
+## Login
+When you see the login page, the default password is `bitmark-webgui`. You can modify the password later after you login.
 
-Note: For local chain, you might need to prepare a miner, otherwise you cannot do transfer bitmark without it
+## Start Bitmark Node
+Choose the bitmark chain you would like to join.
+- `testing`: this chain provides you a playground of bitmark, like bitcoin testnet
+- `bitmark`: connect to real bitmark network, issue and transfer your digital property
 
-### step 1: Create New Bitmark Account or access existing one
-If you don't have any bitmark account, please click the create button to create a new bitmark account. If you already have a bitmark account and want to access it, click the other button.
+After you start the bitmark node, you will send to the Node page that will show you the information and configuration of the bitmark node.
 
-#### Create new bitmark account:
-1. If you decide to create a new bitmark account, then you need to choose the bitmark chain you'd like to join first, then the bitmark-webgui will generate an keypair for you. Store the privateKey in a safe place, if you lost the privateKey, you will lost the account permanently and there is no way to get it back.
-2. After you save your account privateKey, press next, and you will need to type a passcode to protect the privateKey for this session.
-3. After you setup the passcode, you will go to the main page. The page shows default bitmarkd config. (go to step 2)
+## Issue/Transfer bitmark through `bitmark-webgui`
+Click the `Console` button on the navigator bar, you will see a console there. Type the following command:
+`./bin/bitmark-onestep.sh`
 
-#### Access existing bitmark account:
-This is a very simple page, select the chain you would like to join, fill in your privateKey and passcode, then you can press Go button and go to the main page.
-Note: Make sure you access the privateKey to the same chain when the time the key was created
+- The script will ask you what bitmark chain you would like to use, please select the same chain of your bitmark node
+- Please select the `Setup` service for the first time
 
-### step 2: Setup bitmark config
-1. For `local` and `testing` chain, we need to get the bitcoin address to setup the bitmark first. It's simple to get the bitcoin address, click `Issue` in the nav bar and you will see there shows you a bitcoin address, please copy it.
-2. Clicking `Config` button to go to the `edit` page and make changes.
+### `Setup` service
+- Please write down your private key, and send bitcoin to the `wallet address`. If your chain is `testnet`, please contact us to get bitcoins.
 
-#### for local chain:
-- In `Bitcoin` setction, paste the `Bitcoin Address` you just copied into `address` field
-- Click `Save`, then you will go back to the main page
+### `Issue` service
+- The script will help you to issue a file and pay for the bitmark
 
-#### for testing chain:
-- In `Bitcoin` section, there are 3 ways to link to bitcoin:
-1. `local-bitcoin`: link to your local bitcoin. The `bitcoind` must link to bitcoin network in bitmark
-2. `proxy`: link to a bitcoin proxy. The proxy must link to bitcoin network in bitmark
-3. `BitmarkInc-proxy`: proxy provided by Bitmark, connect to bitcoin network in bitmark
-
-- In `Bitcoin` setction, paste the `Bitcoin Address` you just copied into `address` field
-- Click `Save`
-
-#### for bitmark chain:
-- In `Bitcoin` section, there are 3 ways to link to bitcoin:
-
-1. `local-bitcoin`: link to your local bitcoin. The `bitcoind` must link to bitcoin network in bitmark
-2. `proxy`: link to a bitcoin proxy. The proxy must link to bitcoin network in bitmark
-3. `BitmarkInc-proxy`: proxy provided by Bitmark, connect to bitcoin network in bitmark
-
-- In `Bitcoin` setction, paste the `Bitcoin Address` you just copied into `address` field, or you can use your exsiting bitcoin address in livenet
-- Click `Save`
-
-
-### step 3: Start bitmarkd
-Go to the main page by clicking the `bitmark` in navigator bar:
-- Check all the bitmark configuration again
-- Click `start`
-- Waiting until the `mode` field in `Bitmark Info` becomes `Normal`
-
-## Bitmark by yourself - Do issue and transfer
-### environment checking
-- make sure the bicoind is running
-- the `bitmarkd` is running
-- the `bitmarkd` status is `Normal`
-
-### prerequisites
-#### for Local chain
-Before you do issue, you need to send some bitcoin to your `Bitcoin Address`:
-- get the bitmarkNode container bash
-```
-sudo docker exec -it bitmarkNode bash
-```
-- generate bitcoin first
-```
-bitcoin-cli generate 101
-```
-- send bitcoin to your `Bitcoin Address`
-```
-bitcoin-cli sendtoaddress <bitcoinAddress> 10.0
-```
-- generate 3 blocks to make your bitcoin address receive the coins
-```
-bitcoin-cli generate 3
-```
-
-#### for Testing Chain
-- Asking bitmark to send some bitcoin to your address
-
-#### for Bitmark
-- Send some bitcoin to the bitcoin address
-
-### issue/transfer bitmark through `bitmark-webgui`
-Go to the `Issue and Transfer Bitmark` page by clicking the `Issue&Transfer` in navigator bar:
-- Check the balance in `Info` section
-- Fill up the fields in `Issue Asset` / `Transfer Bitmark` section and click `Submit`
+### `Transfer` service
+- The script will help you to transfer a txid to another specified user
--- Docker/bin/bitmark-onestep.sh
+++ Docker/bin/bitmark-onestep.sh
@@ -1,271 +1,256 @@
 #!/bin/sh
 ERROR()
 {
-  echo error: $*
+  echo ERROR: $*
   exit 1
 }
 
-USAGE()
+EXIT()
 {
-  [ -z "$1" ] || echo error: $*
-  echo usage: $(basename "$0") '[options] <commands> ...'
-  echo '(* = required)'
-  echo ''
-  echo '       --help               -h             this message'
-  echo '       --network=net        -n net         Connect to which bitmark network bitmark|testing|local [testing]'
-  echo '       --cli-config=dir     -c dir        *bitmark-cli config directory'
-  echo '       --pay-config=file    -w file       *bitmark-pay config file'
-  echo '       --identity=name      -i name        bitmark-cli identity [default identity]'
-  echo '       --password=pwd       -p pwd         bitmark-cli identity password'
-  echo '       --pay-password=pwd   -s pwd         bitmark-pay password'
-  echo ''
-  echo ' setup  setup bitmark-onestep     '
-  echo '       --connect=IP:PORT    -x IP:PORT    *bitmarkd host/IP and port, HOST:PORT'
-  echo '       --description=desc   -d desc       *bitmark-cli identity description'
-  echo ''
-  echo ' issue  issue and pay bitmark'
-  echo '       --asset=name         -a name       *asset name'
-  echo '       --description=desc   -d desc       *asset description'
-  echo '       --fingerprint=fp     -f fp         *asset fingerprint'
-  echo '       --quantity=num       -q num         quantity to issue [1]'
-  echo ''
-  echo ' transfer transfer and pay bitmark'
-  echo '       --txid=txid          -t txid       *transaction id to transfer'
-  echo '       --receiver=name      -r name       *identity name to receive the transaction'
-  exit 1
+  echo $*
+  exit 0
 }
 
+network=""
 
-network="testing"
-cliConfig=""
-payConfig=""
-identity=""
-password=""
-payPassword=""
-connect=""
-description=""
-asset=""
-fingerprint=""
-quantity=1
-txid=""
-receiver=""
+Welcome()
+{
+  echo ''
+  echo 'Welcome to use bitmark command line service'
+  echo 'Please choose the bitmark network to join:'
+  echo '1) testing'
+  echo '2) bitmark'
+  echo ''
+  read -p 'join to: ' network
+  echo ''
 
-getopt=/usr/local/bin/getopt
-[ -x "${getopt}" ] || getopt=getopt
-args=$(${getopt} -o hp:s:n:c:w:i:x:d:a:f:q:t:r: --long=help,password:,pay-password:,network:,cli-config:,pay-config:,identity:,connect:,description:,asset:,fingerprint:,quantity:,txid:,receiver: -- "$@") ||exit 1
-
-eval set -- "${args}"
-
-# parse options
-while :
-do
-  case "$1" in
-    (-p|--password)
-      password="$2"
-      [ -z "${password}" ] && USAGE password cannot be blank
-      shift
-      ;;
-    (-s|--pay-password)
-      payPassword="$2"
-      [ -z "${payPassword}" ] && USAGE password cannot be blank
-      shift
-      ;;
-    (-n|--network)
-      network="$2"
-      [ "testing" = "${network}" -o "bitmark" = "${network}" -o "local" = "${network}" ] ||   ERROR unrecognized network: ${network}
-      shift
-      ;;
-    (-c|--cli-config)
-      cliConfig="$2"
-      [ -z "${cliConfig}" ] && USAGE cli-config cannot be blank
-      shift
-      ;;
-    (-w|--pay-config)
-      payConfig="$2"
-      [ -z "${payConfig}" ] && USAGE pay-config cannot be blank
-      shift
-      ;;
-    (-i|--identity)
-      identity="$2"
-      [ -z "${identity}" ] && USAGE identity cannot be blank
-      shift
-      ;;
-    (-x|--connect)
-      connect="$2"
-      [ -z "${connect}" ] && USAGE connect cannot be blank
-      shift
-      ;;
-    (-d|--description)
-      description="$2"
-      [ -z "${description}" ] && USAGE description cannot be blank
-      shift
-      ;;
-    (-a|--asset)
-      asset="$2"
-      [ -z "${asset}" ] && USAGE asset cannot be blank
-      shift
-      ;;
-    (-f|--fingerprint)
-      fingerprint="$2"
-      [ -z "${fingerprint}" ] && USAGE fingerprint cannot be blank
-      shift
-      ;;
-    (-q|--quantity)
-      quantity="$2"
-      [ -z "${quantity}" ] && USAGE quantity cannot be blank
-      shift
-      ;;
-    (-t|--txid)
-      txid="$2"
-      [ -z "${txid}" ] && USAGE txid cannot be blank
-      shift
-      ;;
-    (-r|--receiver)
-      receiver="$2"
-      [ -z "${receiver}" ] && USAGE receiver cannot be blank
-      shift
-      ;;
-    (--)
-      shift
-      break
-      ;;
-    (-h|--help)
-      USAGE
-      ;;
-    (*)
-      USAGE invalid option: $1
-      ;;
+  case ${network} in
+    1) echo 'testing'
+       network='testing'
+       ;;
+    2) echo 'bitmark'
+       network='bitmark'
+       ;;
+    *) ERROR invalid network ;;
   esac
-  shift
-done
 
-# validate arguments
-[ $# -ne 1 ] && USAGE invalid arguments
+  InitVariable
 
-# check general required options
-[ -z "${cliConfig}" ] && ERROR cli-config is required
-[ -z "${payConfig}" ] && ERROR pay-config is required
+  service=""
+  echo 'Please choose the service: '
+  echo '1) Setup'
+  echo '2) Issue an bitmark'
+  echo '3) Transfer an bitmark'
+  echo ''
+  read -p 'service: ' service
+  echo ''
+
+  case ${service} in
+    1) Setup ;;
+    2) Issue ;;
+    3) Transfer ;;
+    *) ERROR Service not support ;;
+  esac
+}
 
 bin='./bin'
 bitmarkCli=bitmark-cli
 bitmarkPay="java -jar -Dorg.apache.logging.log4j.simplelog.StatusLogger.level=OFF ${bin}/bitmarkPayService"
+cliConfig=""
+payConfig=""
+identity="admin"
 
-cleanup()
+InitVariable()
 {
-  payPassword=""
-  stty echo
+  cliConfig="./config/bitmark-cli/bitmark-cli-${network}.conf"
+  payNetwork=`echo "$network" | awk '{print toupper($0)}'`
+  payConfig="./config/bitmark-pay/bitmark-pay-${payNetwork}.xml"
 }
-trap cleanup INT EXIT
 
-getPayPassword()
+Setup ()
 {
-  stty -echo
-  if [ -z "${payPassword}" ]
+  [ -f "${cliConfig}" ] && RemoveFile ${cliConfig} && echo ''
+  [ -f "${cliConfig}" ] && EXIT Abort setup
+
+  connect="127.0.0.1:2130"
+  description="admin"
+
+  cliFlags="--config ${cliConfig}"
+  cliFlags="${cliFlags} --identity ${identity}"
+  cliPassword=""
+
+  # get and verify password
+  while [ -z cliPassword -o ${#cliPassword} -lt 8 ]
+  do
+    cliPassword=$(GetPassword 'password (>=8)')
+    if [ -z cliPassword -o ${#cliPassword} -lt 8 ]
+    then
+      echo ''
+      echo 'invalid password'
+      echo ''
+    fi
+  done
+
+  echo ''
+  verifyPassword=$(GetPassword 'verify password')
+  echo ''
+  if [ "${cliPassword}" != "${verifyPassword}" ]
   then
-    read -p "pay password: " payPassword
+    ERROR verify password fail
+  fi
+
+  # setup bitmark-cli
+  echo '---- setup bitmark-cli ----'
+  ${bitmarkCli} ${cliFlags} --password "${cliPassword}" setup --network ${network} --connect ${connect} --description ${description} || rm ${cliConfig}
+  echo ''
+
+  keyPairResult=$(${bitmarkCli} ${cliFlags} --password "${cliPassword}" keypair)
+  privateKey=$(echo "${keyPairResult}" | jq -r '.private_key')
+  echo 'Please right down your private key to a secure place. There is no way to get the private key back.'
+  echo "${privateKey}"
+  echo ''
+  read -p 'type any key to continue ...' c
+
+  # setup bitmark-pay
+  echo '---- setup bitmark-pay ----'
+  ${bitmarkPay} --net="${network}" --config="${payConfig}" --password="${privateKey}"  encrypt
+  echo ''
+  ${bitmarkPay} --net="${network}" --config="${payConfig}" info
+  echo ''
+
+  # clean password and private key
+  cliPassword=''
+  verifyPassword=''
+  keyPairResult=''
+  privateKey=''
+}
+
+RemoveFile()
+{
+  response="N"
+  read -p 'config file is existed, do you want to delete it? (y/N) ' response
+  if [ "${response}" = "y" ]
+  then
+    echo "Remove file ..."
+    rm $1
+  fi
+}
+
+GetPassword()
+{
+  password=''
+  stty -echo
+  if [ -z "${password}" ]
+  then
+    read -p "$1: " password
   fi
   stty echo
-  echo ''
+  echo ${password}
 }
 
-# set bitmark-pay network
-payNetwork=''
-case ${network} in
-  (bitmark)
-    payNetwork='bitmark'
-    ;;
-  (testing)
-    payNetwork='testing'
-  ;;
-  (local)
-    payNetwork='local_bitcoin_reg'
-    ;;
-  (*)
-    ERROR invalid network arguments
-    ;;
-esac
 
-cliFlags="--config ${cliConfig}"
-payFlags="--net=${payNetwork} --config=${payConfig}"
-
-# if password is not empty, add it to cliFlags
-if [ -n "${password}" ] 
-then
-  cliFlags="${cliFlags} --password ${password}"
-fi
-
-# if payPassword is not empty, add it to payFlags
-if [ -n "${payPassword}" ]
-then
-  payFlags="${payFlags} --password=${payPassword}"
-fi
-
-# run command
-case $1 in
-  (setup)
-    # setup bitmark-cli
-    [ -z "${connect}" ] && ERROR connect is required
-    [ -z "${description}" ] && ERROR description is required
-    [ -z "${identity}" ] && ERROR identity is required
-    cliFlags="${cliFlags} --identity ${identity}"
-    echo '---- setup bitmark-cli ----'
-    ${bitmarkCli} ${cliFlags} setup --network ${network} --connect ${connect} --description ${description}
-
-    # setup bitmark-pay
-    echo '---- setup bitmark-pay ----'
-    ${bitmarkPay} ${payFlags} encrypt
-    ${bitmarkPay} ${payFlags} address
-    ;;
-  (issue)
-    # check issue options
-    [ -z "${identity}" ] && ERROR identity is required
-    cliFlags="${cliFlags} --identity ${identity}"
-    [ -z "${asset}" ] && ERROR asset is required
-    [ -z "${description}" ] && ERROR description is required
-    [ -z "${fingerprint}" ] && ERROR fingerprint is required
-    [ ${quantity} -le 0 ] && ERROR invalid quantity
-
-    echo '---- issue asset ----'
-    result=$(${bitmarkCli} ${cliFlags} issue --asset ${asset} --description ${description} --fingerprint ${fingerprint} --quantity ${quantity})
-    echo "${result}"
-    issueIds=$(echo "${result}" | jq -r '.issueIds')
-    paymentAddress=$(echo "${result}" | jq -r '.paymentAddress[0].address')
-    [ -z "${paymentAddress}" ] && ERROR issue asset failed
-
-    # pay the issue ids to the address
-    payFlags="${payFlags} --stdin"
-    echo '---- pay issues ----'
-    getPayPassword
-    for issueId in $(echo ${issueIds} | jq -r '.[]')
+Issue()
+{
+  # get issue field
+  while [ true ]
+  do
+    asset=$(GetRequiredField 'asset name')
+    description=$(GetRequiredField 'description')
+    filePath=$(GetRequiredField 'file path')
+    filePath=$(realpath "${filePath}")
+    while [ ! -f "${filePath}" ]
     do
-      echo "---- paying ${issueId} ----"
-      printf '%s\n' "${payPassword}" | ${bitmarkPay} ${payFlags} pay "${issueId}" "${paymentAddress}"
+      echo "${filePath} not exists"
+      filePath=$(GetRequiredField 'file path')
+      filePath=$(realpath "${filePath}")
     done
-    cleanup
-    ;;
-  (transfer)
-    # check transfer options
-    [ -z "${txid}" ] && ERROR txid is required
-    [ -z "${receiver}" ] && ERROR receiver is required
-    [ -n "${identity}" ] && cliFlags="${cliFlags} --identity ${identity}"
+    quantity=$(GetRequiredField 'quantity')
 
-    echo "---- transfer ${txid} ----"
-    result=$(${bitmarkCli} ${cliFlags} transfer --txid ${txid} --receiver ${receiver})
-    echo "${result}"
-    transferId=$(echo "${result}" | jq -r '.transferId')
-    paymentAddress=$(echo "${result}" | jq -r '.paymentAddress[0].address')
-    [ -z "${paymentAddress}" ] && ERROR transfer transaction failed
-    echo "${paymentAddress}"
+    echo ''
+    echo '==== asset info ===='
+    echo "asset name: ${asset}"
+    echo "description: ${description}"
+    echo "file: ${filePath}"
+    echo "quantity: ${quantity}"
+    echo ''
+    read -p 'issue this asset? (y/N) ' issue
+    [ "y" = "${issue}" ] && break
+  done
 
-    # pay to the address
-    payFlags="${payFlags} --stdin"
-    echo '--- pay transfer ----'
-    getPayPassword
-    echo "---- paying ${transferId} ----"
-    printf '%s\n' "${payPassword}" | ${bitmarkPay} ${payFlags} pay "${transferId}" "${paymentAddress}"
-    cleanup
-    ;;
-  (*)
-    USAGE invalid command: $1
-    ;;
-esac
+  fingerprint=$(sha512sum "${filePath}")
+  fingerprint=$(echo "${fingerprint}" | awk -F ' ' '{print $1}')
+
+  cliFlags="--config ${cliConfig}"
+  cliFlags="${cliFlags} --identity ${identity}"
+
+  cliPassword=$(GetPassword 'password')
+
+  echo ''
+  echo '---- issue asset ----'
+  result=$(${bitmarkCli} ${cliFlags} --password ${cliPassword} issue --asset ${asset} --description ${description} --fingerprint ${fingerprint} --quantity ${quantity})
+  echo "${result}"
+  issueIds=$(echo "${result}" | jq -r '.issueIds')
+  paymentAddress=$(echo "${result}" | jq -r '.paymentAddress[0].address')
+  [ -z "${paymentAddress}" ] && ERROR issue asset failed
+
+  # pay the issue ids to the address
+  keyPairResult=$(${bitmarkCli} ${cliFlags} --password "${cliPassword}" keypair)
+  privateKey=$(echo "${keyPairResult}" | jq -r '.private_key')
+
+  echo ''
+  echo '---- pay issues ----'
+  for issueId in $(echo ${issueIds} | jq -r '.[]')
+  do
+    echo "---- paying ${issueId} ----"
+    printf '%s\n' "${payPassword}" | ${bitmarkPay} --net="${network}" --config="${payConfig}" --password="${privateKey}" pay "${issueId}" "${paymentAddress}"
+  done
+
+  # cleanup
+  cliPassword=''
+  privateKey=''
+}
+
+Transfer()
+{
+  # check transfer options
+  echo ''
+  echo 'Please enter the txId you would like to transfer'
+  txid=$(GetRequiredField 'txid')
+  receiver=$(GetRequiredField 'receiver')
+
+  cliFlags="--config ${cliConfig}"
+  cliFlags="${cliFlags} --identity ${identity}"
+
+  echo "---- transfer ${txid} ----"
+  result=$(${bitmarkCli} ${cliFlags} transfer --txid ${txid} --receiver ${receiver})
+  echo "${result}"
+  transferId=$(echo "${result}" | jq -r '.transferId')
+  paymentAddress=$(echo "${result}" | jq -r '.paymentAddress[0].address')
+  [ -z "${paymentAddress}" ] && ERROR transfer transaction failed
+  echo "${paymentAddress}"
+
+  exit 0
+  # pay to the address
+  payFlags="${payFlags} --stdin"
+  echo '--- pay transfer ----'
+  echo "---- paying ${transferId} ----"
+  printf '%s\n' "${payPassword}" | ${bitmarkPay} ${payFlags} pay "${transferId}" "${paymentAddress}"
+}
+
+GetRequiredField()
+{
+  value=''
+  while [ -z "${value}" ]
+  do
+    read -p "$1: " value
+    if [ -z "${value}" ]
+    then
+      echo "$1 is required" >&2
+      echo '' >&2
+    fi
+  done
+
+  echo $value
+}
+
+Welcome
